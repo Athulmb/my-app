08b97c707cc58116e6bce9b65830127851b13725
Athulmb <athulmb66@gmail.com>
2025-08-25 17:56:05 +0530

    todays work blog

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from "./Pages/Layout";
-
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
 import About from "./Pages/About";
-
-
-
-
+import Blog from "./Pages/Blog";
+import Error404 from "./Pages/Error404";
+import PrivacyPolicy from "./Pages/PrivacyPolicy";
 
 const App = () => {
   return (
@@ -18,11 +16,15 @@ const App = () => {
         <Routes>
           {/* Home page with multiple sections */}
           <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
 
 
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
 
+          {/* ✅ Catch-all route for 404 */}
+          <Route path="*" element={<Error404/>} />
         </Routes>
       </Layout>
     </Router>
